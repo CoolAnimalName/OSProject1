adb0ce8eaef016f91026b70d31dd5b4623c38d68
CoolAnimalName <mattvillarr@gmail.com>
2017-02-19 23:06:00 -0600

    Some basic functionality works

--- OSProject1/CPU.java
+++ OSProject1/CPU.java
@@ -14,26 +14,54 @@ public class CPU {
 	  	InputStream is = proc.getInputStream();
 	  	OutputStream os = proc.getOutputStream();
 			Scanner sc = new Scanner(is);
+      PrintWriter pw = new PrintWriter(os);
 
       int ir = -1; //init
-      int pc = 0, sp = 0, ac = 0, x = 0, y = 0;
+      int pc = 0, sp = 0, ac = -1, x = 0, y = 0;
       Stack<Integer> user = new Stack<>(); //user stack
       Stack<Integer> sys = new Stack<>(); //system stack
 
+      pw.println(pc); //initial fetch
+      pw.flush();
+      pc++;
+
 			loop: while(sc.hasNextLine()) {
 				ir = Integer.parseInt(sc.nextLine());
-        pc++;
+        //System.out.println(ir);
+        //System.out.println("pc = " + pc);
 				switch(ir) {
 					case 1: //Load value
+            pw.println(pc);
+            pw.flush();
             ac = Integer.parseInt(sc.nextLine());
+            pc++;
 						break;
 					case 2: //Load addr
-            int temp = Integer.parseInt(sc.nextLine());
-            //ac =
+            pw.println(pc);
+            pw.flush();
+            ac = Integer.parseInt(sc.nextLine());
+            pw.println(ac); //gets the value at the address
+            pw.flush();
+            ac = Integer.parseInt(sc.nextLine());
+            pc++;
 						break;
 					case 3: //LoadInd addr
+            pw.println(pc);
+            pw.flush();
+            ac= Integer.parseInt(sc.nextLine());
+            pw.println(ac); //gets the value at the address
+            pw.flush();
+            ac = Integer.parseInt(sc.nextLine());
+            pc++;
 						break;
 					case 4: //LoadIdx X addr
+            pw.println(pc);
+            pw.flush();
+            ac= Integer.parseInt(sc.nextLine());
+            pw.println(ac + x); //gets the value at the address
+            pw.flush();
+            ac = Integer.parseInt(sc.nextLine());
+            pc++;
 						break;
 					case 5: //LoadIdx Y addr
 						break;
@@ -46,11 +74,14 @@ public class CPU {
             ac = r.nextInt(100) + 1;
 						break;
           case 9: //Put port
+            pw.println(pc);
+            pw.flush();
             int port = Integer.parseInt(sc.nextLine());
             if(port == 1)
               System.out.println(ac);
             else if(port == 2)
               System.out.println((char) ac);
+            pc++;
             break;
           case 10: //AddX
             ac += x;
@@ -83,15 +114,27 @@ public class CPU {
             sp = ac;
             break;
           case 20: //Jump addr
-
+            pw.println(pc);
+            pw.flush();
+            pc = Integer.parseInt(sc.nextLine()); //jumps
             break;
           case 21: //JumpIfEqual addr
-            if(ac == 0){}
-              //something
+            if(ac == 0){
+              pw.println(pc);
+              pw.flush();
+              pc = Integer.parseInt(sc.nextLine()); //jumps
+            }
+            else
+              pc++;
             break;
           case 22: //JumpIfNotEqual addr
-            if(ac != 0){}
-              //something
+            if(ac != 0){
+              pw.println(pc);
+              pw.flush();
+              pc= Integer.parseInt(sc.nextLine()); //jumps
+            }
+              else
+                pc++;
             break;
           case 23: //Call addr
             break;
@@ -115,10 +158,12 @@ public class CPU {
             break loop;
           default:
             System.out.println("ERROR! Cannot compute");
-            break loop; //System.exit(0);
+            System.exit(0);//break loop;
 				} //end switch-case
-				System.out.println(ir);
-			} //end while
+        pw.println(pc);
+        pw.flush();
+        pc++;
+			} //end loop
 
 			proc.waitFor();
 			int exitVal = proc.exitValue();
--- OSProject1/Memory.java
+++ OSProject1/Memory.java
@@ -4,18 +4,21 @@ import java.util.Scanner;
 public class Memory {
   public static void main(String args[]) {
     File inputFile = new File(args[0]);
-    int fetch = Integer.parseInt(args[1]);
+    int timer = Integer.parseInt(args[1]);
     final int SYSTEM = 1000;
     int mem[] = new int[2000];
 
     try {
       Scanner scan = new Scanner(inputFile);
-      int i =0;
+      Scanner fetch = new Scanner(System.in);
+      int i =0, j =0;
       while(scan.hasNextInt()) {
         mem[i++] = scan.nextInt();
         scan.nextLine(); //ignore the comments
-      }
-      for(int j = 0; j < i; j++) {
+      } //end while
+
+      while(fetch.hasNext()) {
+        j = fetch.nextInt();
         System.out.println(mem[j]);
       }
     } //end try
