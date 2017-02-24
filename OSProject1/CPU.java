60cb0fb911f8c7d5dbc57ecd46201c8e5a0888a1
CoolAnimalName <mattvillarr@gmail.com>
2017-02-23 21:03:56 -0600

    all samples work

--- OSProject1/CPU.java
+++ OSProject1/CPU.java
@@ -1,189 +1,410 @@
 import java.io.*;
-import java.util.Scanner;
-import java.util.Stack;
-import java.lang.Runtime;
-import java.util.Random;
+import java.util.*;
 
 public class CPU {
-  public static void main(String args[]) {
-		try {
-	  	Runtime rt = Runtime.getRuntime();
-			String run = "java Memory " + args[0] + " " + args[1];
-	  	Process proc = rt.exec(run);
 
-	  	InputStream is = proc.getInputStream();
-	  	OutputStream os = proc.getOutputStream();
-			Scanner sc = new Scanner(is);
-      PrintWriter pw = new PrintWriter(os);
 
-      int ir = -1; //init
-      int pc = 0, sp = 0, ac = -1, x = 0, y = 0;
-      Stack<Integer> user = new Stack<>(); //user stack
-      Stack<Integer> sys = new Stack<>(); //system stack
+  private static int PC = 0;
+  private static int SP = 1000;
+  private static int IR = 0;
+  private static int AC = 0;
+  private static int X = 0;
+  private static int Y = 0;
+  private static int timer = 0;
+  private static int topOfUserStack = 1000;
+  private static int topOfSystemStack = 2000;
+  private static int numInstructions = 0;
 
-      pw.println(pc); //initial fetch
-      pw.flush();
-      pc++;
+  private static String filename;
+  private static OutputStream out;
+  private static PrintWriter printW;
+  private static InputStream in;
 
-      loop: while(sc.hasNextLine()) {
-        ir = Integer.parseInt(sc.nextLine());
-        //System.out.println(ir);
-        //System.out.println("pc = " + pc);
-        switch(ir) {
-          case 1: //Load value
-            pw.println(pc);
-            pw.flush();
-            ac = Integer.parseInt(sc.nextLine());
-            pc++;
-            break;
-          case 2: //Load addr
-            pw.println(pc);
-            pw.flush();
-            ac = Integer.parseInt(sc.nextLine());
-            pw.println(ac); //gets the value at the address
-            pw.flush();
-            ac = Integer.parseInt(sc.nextLine());
-            pc++;
-            break;
-          case 3: //LoadInd addr
-            pw.println(pc);
-            pw.flush();
-            ac = Integer.parseInt(sc.nextLine());
-            pw.println(ac); //gets the value at the address
-            pw.flush();
-            ac = Integer.parseInt(sc.nextLine());
-            pc++;
-            break;
-          case 4: //LoadIdx X addr
-            pw.println(pc);
-            pw.flush();
-            ac = Integer.parseInt(sc.nextLine());
-            pw.println(ac + x); //gets the value at the address
-            pw.flush();
-            ac = Integer.parseInt(sc.nextLine());
-            pc++;
-            break;
-          case 5: //LoadIdx Y addr
-            pw.println(pc);
-            pw.flush();
-            ac= Integer.parseInt(sc.nextLine());
-            pw.println(ac + y); //gets the value at the address
-            pw.flush();
-            ac = Integer.parseInt(sc.nextLine());
-            pc++;
-            break;
-          case 6: //LoadSp X
-            pw.println(sp + x);
-            pw.flush();
-            ac= Integer.parseInt(sc.nextLine());
-            pc++;
-            break;
-          case 7: //Store addr
-            break;
-          case 8: //Get
-            Random r = new Random();
-            ac = r.nextInt(100) + 1;
-            break;
-          case 9: //Put port
-            pw.println(pc);
-            pw.flush();
-            int port = Integer.parseInt(sc.nextLine());
-            if(port == 1)
-              System.out.print(ac);
-            else if(port == 2)
-              System.out.print((char) ac);
-            pc++;
-            break;
-          case 10: //AddX
-            ac += x;
-            break;
-          case 11: //AddY
-            ac += y;
-            break;
-          case 12: //SubX
-            ac -= x;
-            break;
-          case 13: //SubY
-            ac -= y;
-            break;
-          case 14: //CopyToX
-            x = ac;
-            break;
-          case 15: //CopyFromX
-            ac = x;
-            break;
-          case 16: //CopytoY
-            y = ac;
-            break;
-          case 17: //CopyFromY
-            ac = y;
-            break;
-          case 18: //CopyToSp
-            sp = ac;
-            break;
-          case 19: //CopyFromSp
-            sp = ac;
-            break;
-          case 20: //Jump addr
-            pw.println(pc);
-            pw.flush();
-            pc = Integer.parseInt(sc.nextLine()); //jumps
-            break;
-          case 21: //JumpIfEqual addr
-            if(ac == 0){
-              pw.println(pc);
-              pw.flush();
-              pc = Integer.parseInt(sc.nextLine()); //jumps
-            }
-            else
-              pc++;
-            break;
-          case 22: //JumpIfNotEqual addr
-            if(ac != 0){
-              pw.println(pc);
-              pw.flush();
-              pc= Integer.parseInt(sc.nextLine()); //jumps
-            }
-              else
-                pc++;
-            break;
-          case 23: //Call addr
-            break;
-          case 24: //Ret
-            break;
-          case 25: //IncX
-            x++;
-            break;
-          case 26: //DecX
-            x--;
-            break;
-          case 27: //Push
-            break;
-          case 28: //Pop
-            break;
-          case 29: //Int
-            break;
-          case 30: //IRet
-            break;
-          case 50: //End
-            pw.println(-1);
-            pw.flush();
-            break loop;
-          default:
-            System.out.println("ERROR! Cannot compute");
-            System.exit(0);//break loop;
-				} //end switch-case
-        pw.println(pc);
-        pw.flush();
-        pc++;
-			} //end loop
+  private static boolean interrupt = false;
+  private static boolean UKmode = true;        //usermode = true, kernalmode = false
 
-			proc.waitFor();
-			int exitVal = proc.exitValue();
-			System.out.println("Process exited: " + exitVal);
-		} //end try
-		catch(Throwable t) {
-			t.printStackTrace();
-		} //end catch
-	} //end main
-} //end CPU
+  public static void main(String args[])
+  {
+    int instruction = 0;
+
+    //*********************************************
+    //              GET ARGUMENTS                 *
+    //*********************************************
+
+    // Validates if the correct number of arguments is 2
+    if(args.length == 2)
+    {
+      filename = args[0];
+      timer = Integer.parseInt(args[1]);
+    }
+    else
+    {  // If number of arguments is not 2, then exit with error.
+      System.out.println("ERROR: Need only two parameters.");
+      System.exit(0);
+    }
+
+    //*********************************************
+    //           CREATE PROCESS                   *
+    //*********************************************
+
+    try{
+
+      // Start creating child process
+      Runtime runtime = Runtime.getRuntime();
+      Process process = runtime.exec("java Memory");
+      out = process.getOutputStream();
+      printW = new PrintWriter(out);
+      in = process.getInputStream();
+      Scanner memory = new Scanner(in);
+
+      // This sends the filename to memory
+      printW.printf(filename + "\n");
+      printW.flush();
+
+      while(true)
+      {
+        // If a timer interrupt has occured
+        if(interrupt == false && numInstructions > 0 && (numInstructions % timer) == 0)
+        {
+          interrupt = true;
+          UKmode = false;
+          int tempData;
+
+          tempData = SP;
+          SP = topOfSystemStack;
+          push(in, out, printW, tempData);
+          tempData = PC;
+          PC = 1000;
+          push(in, out, printW, tempData);
+        }
+
+        // Starts reading in the instructions from memory
+        instruction = readMemory(memory, in, out, printW, PC);
+
+        // Test if instructions are correct
+        //System.out.println(instruction);
+
+
+        if(instruction != -1)
+        {
+          runIntruction(memory, in, out, printW, instruction);
+          //System.out.println(numInstructions);
+
+        }
+        else
+          break;
+
+      }
+
+      // Need it to output this, but it's not...
+      //System.out.println("debug 6: Line 93");
+      process.waitFor();
+      int exitValue = process.exitValue();
+      System.out.println("Process exited: " + exitValue);
+    }
+    catch(IOException e)
+    {
+      e.printStackTrace();
+    }
+    catch(InterruptedException e)
+    {
+      e.printStackTrace();
+    }
+  }// end main
+
+  //*********************************************
+  //                 METHODS                    *
+  //*********************************************
+
+  // readMemory:    Reads data from Memory at given addresses
+  private static int readMemory(Scanner memory, InputStream in, OutputStream out, PrintWriter printW, int address)
+  {
+    String tempString = null;
+    //System.out.println("address: " + address);
+
+    // Address validation: If usermode is tryng to access the system address
+    if(UKmode)
+    {
+      if(address >= 1000 || address < 0)
+      {
+        System.out.println("Error: User cannot access system stack.");
+        System.exit(0);
+      }
+    }
+
+    printW.printf("1," + address + "\n");
+    printW.flush();
+
+    if(memory.hasNext())
+    {
+      tempString = memory.next();
+      if(!tempString.isEmpty())
+      {
+        int tempInt = Integer.parseInt(tempString);
+        return(tempInt);
+      }
+    }
+    return -1;
+  }
+
+  private static void writeMemory(InputStream in, OutputStream out, PrintWriter printW, int address, int value)
+  {
+    printW.printf("2," + address + "," + value + "\n");
+    printW.flush();
+  }
+  private static int pop(Scanner memory, InputStream in, OutputStream out, PrintWriter printW)
+  {
+    int instruction = readMemory(memory, in, out, printW, SP);
+    writeMemory(in, out, printW, SP, 0);
+    SP++;
+    return instruction;
+  }
+
+  private static void push(InputStream in, OutputStream out, PrintWriter printW, int data)
+  {
+    SP--;
+    writeMemory(in, out, printW, SP, data);
+  }
+
+  private static void addInstruction()
+  {
+    if(interrupt == false)
+      numInstructions++;
+  }
+
+  private static void runIntruction(Scanner memory, InputStream in, OutputStream out, PrintWriter printW, int instruction)
+  {
+    IR = instruction;
+    int data;
+
+    switch(IR)
+    {
+      case 1: //  Load value:      Load value into the AC
+        PC++;
+        data = readMemory(memory, in, out, printW, PC);
+        AC = data;
+        addInstruction();
+        PC++;
+        break;
+      case 2:  //  Load addr:      Load value at the address into AC
+        PC++;
+        data = readMemory(memory, in, out, printW, PC);
+        AC = readMemory(memory, in, out, printW, data);
+        addInstruction();
+        PC++;
+        break;
+      case 3:  //  LoadInd addr:   Load value from address found in the given address into the AC
+        PC++;
+        data = readMemory(memory, in, out, printW, PC);
+        data = readMemory(memory, in, out, printW, data);
+        AC = readMemory(memory, in, out, printW, data);
+        addInstruction();
+        PC++;
+        break;
+      case 4:  //  LoadIdxX addr:  Load the value at (address + X) into AC
+        PC++;
+        data = readMemory(memory, in, out, printW, PC);
+        AC = readMemory(memory, in, out, printW, data + X);
+        addInstruction();
+        PC++;
+        break;
+      case 5:  //  LoadIdxY addr:  Load the value at(address + Y) into AC
+        PC++;
+        data = readMemory(memory, in, out, printW, PC);
+        AC = readMemory(memory, in, out, printW, data + Y);
+        addInstruction();
+        PC++;
+        break;
+      case 6:  //  LoadSpX:        Load from (SP + X) into the AC
+        //(if SP is 990, and X is 1, load from 991).
+        AC = readMemory(memory, in, out, printW, SP + X);
+        addInstruction();
+        PC++;
+        break;
+      case 7:  //  Store Addr:     Store value into AC into the address
+        PC++;
+        data = readMemory(memory, in, out, printW, PC);
+        writeMemory(in, out, printW, data, AC);
+        addInstruction();
+        PC++;
+        break;
+      case 8:  //  Get:            Gets a random int from 1 to 100 and put into AC
+        Random intRandom = new Random();
+        int tempInt = intRandom.nextInt(100) + 1;
+        AC = tempInt;
+        addInstruction();
+        PC++;
+        break;
+      case 9:  //  Put port:       Put into the port depending what data is
+        PC++;
+        data = readMemory(memory, in, out, printW, PC);
+
+
+        if(data == 1)                   // If port = 1, writes to AC as an Int
+        {
+          System.out.print(AC);
+          addInstruction();
+          PC++;
+          break;
+        }
+        else if(data == 2)              // Else if, writes to AC as a char
+        {
+          System.out.print((char)AC);
+          addInstruction();
+          PC++;
+          break;
+        }
+        else                            // Else, there was an error at the port
+        {
+          System.out.println("ERROR: Port: " + data);
+          addInstruction();
+          PC++;
+          System.exit(0);
+          break;
+        }
+      case 10:  //  AddX:        Add X to the AC
+        AC = AC + X;
+        addInstruction();
+        PC++;
+        break;
+      case 11:  //  AddY:        Add Y to the AC
+        AC = AC + Y;
+        addInstruction();
+        PC++;
+        break;
+      case 12:  //  SubX:        Subtract the value of X from the AC
+        AC = AC - X;
+        addInstruction();
+        PC++;
+        break;
+      case 13:  //  SubY:        Subtract the value of Y from the AC
+        AC = AC - Y;
+        addInstruction();
+        PC++;
+        break;
+      case 14:  //  CopyToX:     Copy the value in the AC to X
+        X = AC;
+        addInstruction();
+        PC++;
+        break;
+      case 15:  //  CopyFromX:   Copy value in the AC to X
+        AC = X;
+        addInstruction();
+        PC++;
+        break;
+      case 16:  //  CopyToY:     Copy the value in the AC to Y
+        Y = AC;
+        addInstruction();
+        PC++;
+        break;
+      case 17:  //  CopyFromY:   Copy value in the Y to AC
+        AC = Y;
+        addInstruction();
+        PC++;
+        break;
+      case 18:  //  CopyToSP:    Copy the value in AC to SP
+        SP = AC;
+        addInstruction();
+        PC++;
+        break;
+      case 19:  //  CopyFromSP:  Copy the value in SP to AC
+        AC = SP;
+        addInstruction();
+        PC++;
+        break;
+      case 20:  //  Jump addr:   Jump to the address
+        PC++;
+        data = readMemory(memory, in, out, printW, PC);
+        PC = data;
+        addInstruction();
+        break;
+      case 21:  //  JumpIfEqual addr:  Jump to address only if value in the AC = 0
+        PC++;
+        data = readMemory(memory, in, out, printW, PC);
+        if(AC ==0)
+        {
+          PC = data;
+          addInstruction();
+          break;
+        }
+        addInstruction();
+        PC++;
+        break;
+      case 22:  //  JumpIfNotEqual addr:  Jump to address only if value in the AC != 0
+        PC++;
+        data = readMemory(memory, in, out, printW, PC);
+        if(AC != 0)
+        {
+          PC = data;
+          addInstruction();
+          break;
+        }
+        addInstruction();
+        PC++;
+        break;
+      case 23:  //  Call addr:            Push return address onto stack, then jump to the address
+        PC++;
+        data = readMemory(memory, in, out, printW, PC);
+        push(in, out, printW, PC + 1);
+        topOfUserStack = SP;
+        PC = data;
+        addInstruction();
+        break;
+      case 24:  //  Ret:   Pop the return address from the stack, then jump to the address
+        data = pop(memory, in, out, printW);
+        PC = data;
+        addInstruction();
+        break;
+      case 25:  //  IncX:   Increment X
+        X++;
+        addInstruction();
+        PC++;
+        break;
+      case 26:  //  DecX:   Decrement X
+        X--;
+        addInstruction();
+        PC++;
+        break;
+      case 27:  //  Push:   Push AC onto stack
+        push(in, out, printW, AC);
+        PC++;
+        addInstruction();
+        break;
+      case 28:  //  Pop:    Pop from stack into AC
+        AC = pop(memory, in, out, printW);
+        PC++;
+        addInstruction();
+        break;
+      case 29:  //  Int:    Perform system call
+        data = SP;
+        SP = 2000;
+        interrupt = true;
+        UKmode = false;
+        push(in, out, printW, data);
+
+        data = PC + 1;
+        PC = 1500;
+        push(in, out, printW, data);
+        addInstruction();
+
+        break;
+      case 30:  //  IRet:   Return from system call
+        PC = pop(memory, in, out, printW);
+        SP = pop(memory, in, out, printW);
+        UKmode = true;
+        numInstructions++;
+        interrupt = false;
+        break;
+      case 50:  //  End:    End the execution
+        addInstruction();
+        System.exit(0);
+        break;
+      default:
+        System.out.println("ERROR: Invalid Instruction...");
+        System.exit(0);
+        break;
+
+    }
+  }
+
+} // ends class
--- OSProject1/Memory.java
+++ OSProject1/Memory.java
@@ -1,44 +1,144 @@
+// INPUT: java CPU sample.txt 30
+
 import java.io.*;
 import java.util.Scanner;
 
-public class Memory {
-  public static void main(String args[]) {
-    File inputFile = new File(args[0]);
-    int timer = Integer.parseInt(args[1]);
-    final int SYSTEM = 1000;
-    int mem[] = new int[2000];
+public class Memory
+{
+  private static int [] mem = new int[2000];    // Used to store memory
 
-    try {
-      Scanner scan = new Scanner(inputFile);
-      Scanner fetch = new Scanner(System.in);
-      String parse = null;
-      int i =0, j =0;
-      while(scan.hasNext()) {
-        if(scan.hasNextInt()) {
-          mem[i++] = scan.nextInt();
-        } //end if
-        else {
-          parse = scan.next();
-          if(parse.equals("//"))
-            scan.nextLine();
-          else if(parse.charAt(0) == '.')
-            i = Integer.parseInt(parse.substring(1));
-          else
-            scan.nextLine(); //skips \n
-        } //end else
-          scan.nextLine(); //ignore the comments
-      } //end while
+  public static void main (String args[])
+  {
+    Scanner CPU = new Scanner(System.in);    // Used to read input from the CPU
+    File infile = null;                      // Used for filename
+    Scanner input = null;                    // Used to go through file
 
-      //while(fetch.hasNext()) {
-      for(j=0; j<i;j++){
-        //j = fetch.nextInt();
-        //if(j == -1)
-          //break;
-        System.out.println(mem[j]);
+
+    int currentNum = 0;    // Will contain number from file
+    int index = 0;         // Will contain element number of memory array
+    String currentString;  // Will contain a String for comments or '.'
+    String line;           // Will contain the line for the CPU scanner
+    int address;           // Stores the addresss for write method
+    int val;               // Stores value for the write method
+    int num;               // Will contain the int value of the strings for the CPU scanner
+
+    //**********************************************
+    //                  GET FILE                   *
+    //**********************************************
+    try
+    {
+      if(CPU.hasNextLine())
+      {
+        infile = new File(CPU.nextLine());
+
+        // Input Validation: If there is no file, then close.
+        if(!(infile.exists()))
+        {
+          System.out.println("Cannot find file.");
+          System.exit(0);
+        }
       }
-    } //end try
-    catch(FileNotFoundException e){
+
+      //**********************************************
+      //            READ FILE CONTENT                *
+      //**********************************************
+      try
+      {
+        input = new Scanner(infile);
+
+        while(input.hasNext())
+        {
+          // Store int into memory
+          if(input.hasNextInt())
+          {
+            currentNum = input.nextInt();
+            mem[index] = currentNum;
+            index++;
+          }
+          else
+          {
+            currentString = input.next();
+
+
+            if(currentString.equals("//"))
+            {
+              // Skip line if the line is a comment
+              input.nextLine();
+            }
+            else if(currentString.charAt(0) == '.')
+            {
+              // Get value if there is a '.' in the front
+              index = Integer.parseInt(currentString.substring(1));
+            }
+            else
+              input.nextLine();
+          }
+        }
+      }
+      catch(FileNotFoundException e)
+      {
+        e.printStackTrace();
+      }
+      //*********************************************
+      //          EXECUTE READ AND WRITES           *
+      //*********************************************
+
+      // Read instructions and execute
+      // by the CPU read or write functions requested
+      while(true)
+      {
+        if(CPU.hasNext())
+        {
+          line = CPU.nextLine();
+          if(!(line.isEmpty()))
+          {
+            // Testing to see if getting stuff out of file
+            //System.out.println(line);
+
+            // Parses through CPU
+            String [] tokens = line.split(",");
+
+            // A 1 means the CPU wants to read
+            // Else, a 2 means the CPU wants to write
+            if(tokens[0].equals("1"))
+            {
+              num = Integer.parseInt(tokens[1]);
+              System.out.println(read(num));
+            }
+            else
+            {
+              address = Integer.parseInt(tokens[1]);
+              val = Integer.parseInt(tokens[2]);
+              write(address, val);
+            }
+          }
+          else
+            break;
+        }
+        else
+          break;
+      }
+
+    }
+    catch(NumberFormatException e )
+    {
       e.printStackTrace();
-    } //end catch
-  } //end main
-} //end Memory
+    }
+
+  } // end main
+
+  //*********************************************
+  //                 METHODS                    *
+  //*********************************************
+
+  public static int read(int address)
+  {
+    return mem[address];
+  }
+  public static void write(int address, int value)
+  {
+    mem[address] = value;
+  }
+
+
+} // end class
