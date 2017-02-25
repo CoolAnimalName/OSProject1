c656b252244df5de7b36055afb6dbb7aaea09eea
CoolAnimalName <mattvillarr@gmail.com>
2017-02-24 23:35:25 -0600

    more cleanup

--- OSProject1/CPU.java
+++ OSProject1/CPU.java
@@ -28,8 +28,8 @@ public class CPU {
 
   static int instrNum = 0; //number of instructions done before timer
 
-  static OutputStream os;
-  static PrintWriter fetchPW;
+  //static OutputStream os;
+  //static PrintWriter fetchPW;
   static InputStream is;
 
   static boolean interrupt = false;
@@ -49,8 +49,8 @@ public class CPU {
     try {
       Runtime rt = Runtime.getRuntime();
       Process proc = rt.exec("java Memory " + args[0]);
-      os = proc.getOutputStream();
-      fetchPW = new PrintWriter(os);
+      OutputStream os = proc.getOutputStream();
+      PrintWriter fetchPW = new PrintWriter(os);
       is = proc.getInputStream();
       Scanner memory = new Scanner(is);
 
@@ -94,7 +94,7 @@ public class CPU {
 
     if(mode) { //checks to make sure user is not accessing system stack
       if(addr >= 1000 || addr < 0) { //user mem addr is 0-999
-        System.out.println("Error: User cannot access system stack.");
+        System.out.println("Memory violation: accessing system address " + addr + " in user mode ");
         System.exit(0);
       } //end  nested if
     } //end if
@@ -119,6 +119,11 @@ public class CPU {
     fetchPW.flush();
   } //end writeMem
 
+  private static void push(InputStream is, OutputStream os, PrintWriter fetchPW, int data) {
+    sp--;
+    writeMem(is, os, fetchPW, sp, data);
+  } //end push
+
   private static int pop(Scanner memory, InputStream is, OutputStream os, PrintWriter fetchPW) {
     ir = readMem(memory, is, os, fetchPW, sp);
     writeMem(is, os, fetchPW, sp, 0); //zero to reset mem address
@@ -126,56 +131,45 @@ public class CPU {
     return ir;
   } //end pop
 
-  private static void push(InputStream is, OutputStream os, PrintWriter fetchPW, int data) {
-    sp--;
-    writeMem(is, os, fetchPW, sp, data);
-  } //end push
-
   private static void instrIncrement(){
-    if(interrupt == false)
+    if(interrupt == false) //only increment time to interrupt if there is no current interupt
       instrNum++;
   } //end instrIncrement
 
   private static void executeInstr(Scanner memory, InputStream is, OutputStream os, PrintWriter fetchPW, int ir) {
-
     int temp;
+    pc++;
+
     switch(ir) {
       case 1: //  Load value:      Load value into the ac
-        pc++;
         ac = readMem(memory, is, os, fetchPW, pc);
         pc++;
         break;
       case 2:  //  Load addr:      Load value at the addr into ac
-        pc++;
         temp = readMem(memory, is, os, fetchPW, pc);
         ac = readMem(memory, is, os, fetchPW, temp);
         pc++;
         break;
       case 3:  //  LoadInd addr:   Load value from addr found in the given addr into the ac
-        pc++;
         temp = readMem(memory, is, os, fetchPW, pc);
         temp = readMem(memory, is, os, fetchPW, temp);
         ac = readMem(memory, is, os, fetchPW, temp);
         pc++;
         break;
       case 4:  //  LoadIdxx addr:  Load the value at (addr + x) into ac
-        pc++;
         temp = readMem(memory, is, os, fetchPW, pc);
         ac = readMem(memory, is, os, fetchPW, temp + x);
         pc++;
         break;
       case 5:  //  LoadIdxy addr:  Load the value at(addr + y) into ac
-        pc++;
         temp = readMem(memory, is, os, fetchPW, pc);
         ac = readMem(memory, is, os, fetchPW, temp + y);
         pc++;
         break;
       case 6:  //  Loadspx:        Load from (sp + x) into the ac
         ac = readMem(memory, is, os, fetchPW, sp + x);
-        pc++;
         break;
       case 7:  //  Store Addr:     Store value into ac into the addr
-        pc++;
         temp = readMem(memory, is, os, fetchPW, pc);
         writeMem(is, os, fetchPW, temp, ac);
         pc++;
@@ -184,75 +178,54 @@ public class CPU {
         Random rand = new Random();
         int tempInt = rand.nextInt(100) + 1;
         ac = tempInt;
-        pc++;
         break;
       case 9:  //  Put port:       Put into the port depending what temp is
-        pc++;
         int port = readMem(memory, is, os, fetchPW, pc);
 
-        if(port == 1) {
+        if(port == 1)
           System.out.print(ac);
-          pc++;
-          break;
-        } //end if
-        else if(port == 2) {
+        else if(port == 2)
           System.out.print((char)ac);
-          pc++;
-          break;
-        } //end else if
         else {
           System.out.println("ERROR: port cannot be: " + port);
-          instrIncrement();
-          pc++;
           System.exit(0);
-          break;
         } //end else
+        pc++;
+        break;
       case 10:  //  Addx:        Add x to the ac
         ac +=  x;
-        pc++;
         break;
       case 11:  //  Addy:        Add y to the ac
         ac += y;
-        pc++;
         break;
       case 12:  //  Subx:        Subtract the value of x from the ac
         ac -= x;
-        pc++;
         break;
       case 13:  //  Suby:        Subtract the value of y from the ac
         ac -= y;
-        pc++;
         break;
       case 14:  //  CopyTox:     Copy the value in the ac to x
         x = ac;
-        pc++;
         break;
       case 15:  //  CopyFromx:   Copy value in the ac to x
         ac = x;
-        pc++;
         break;
       case 16:  //  CopyToy:     Copy the value in the ac to y
         y = ac;
-        pc++;
         break;
       case 17:  //  CopyFromy:   Copy value in the y to ac
         ac = y;
-        pc++;
         break;
       case 18:  //  CopyTosp:    Copy the value in ac to sp
         sp = ac;
-        pc++;
         break;
       case 19:  //  CopyFromsp:  Copy the value in sp to ac
         ac = sp;
-        pc++;
         break;
       case 20:  //  Jump addr:   Jump to the addr
-        pc++;
         pc = readMem(memory, is, os, fetchPW, pc);
         break;
       case 21:  //  JumpIfEqual addr:  Jump to addr only if value in the ac = 0
-        pc++;
         if(ac ==0) {
           pc = readMem(memory, is, os, fetchPW, pc);
           break;
@@ -260,7 +233,6 @@ public class CPU {
         pc++;
         break;
       case 22:  //  JumpIfNotEqual addr:  Jump to addr only if value in the ac != 0
-        pc++;
         if(ac != 0) {
           pc = readMem(memory, is, os, fetchPW, pc);
           break;
@@ -268,7 +240,6 @@ public class CPU {
         pc++;
         break;
       case 23:  //  Call addr:            Push return addr onto stack, then jump to the addr
-        pc++;
         push(is, os, fetchPW, pc + 1);
         USTACK = sp;
         pc = readMem(memory, is, os, fetchPW, pc);
@@ -278,19 +249,15 @@ public class CPU {
         break;
       case 25:  //  Incx:   Increment x
         x++;
-        pc++;
         break;
       case 26:  //  Decx:   Decrement x
         x--;
-        pc++;
         break;
       case 27:  //  Push:   Push ac onto stack
         push(is, os, fetchPW, ac);
-        pc++;
         break;
       case 28:  //  Pop:    Pop from stack into ac
         ac = pop(memory, is, os, fetchPW);
-        pc++;
         break;
       case 29:  //  Int:    Perform system call
         temp = sp;
@@ -299,7 +266,7 @@ public class CPU {
         mode = false;
         push(is, os, fetchPW, temp);
 
-        temp = pc + 1;
+        temp = pc;
         pc = 1500;
         push(is, os, fetchPW, temp);
         break;
